eae3febf42b8be117fe815d3b217a34c6a71be13
Sebastian Borrazas <seba.borrazas@gmail.com>
2018-07-21 22:15:28 -0300

    Use global store for vectors usage for now

--- src/shen_erl_global_stores.erl
+++ src/shen_erl_global_stores.erl
@@ -9,11 +9,19 @@
          get_mfa/1,
          set_mfa/2,
          get_val/1,
-         set_val/2]).
+         set_val/2,
+         init_vector/1,
+         set_vector_val/3,
+         get_vector_val/2]).
 
 %% Macros
 -define(FUNCTIONS_STORE_NAME, '_kl_funs_store').
 -define(VALUES_STORE_NAME, '_kl_values_store').
+-define(VECTORS_STORE_NAME, '_kl_vectors_store').
+-define(VECTOR_MAX_ID, 4294967296).
+
+%% Types
+-opaque vector() :: non_neg_integer().
 
 %%%===================================================================
 %%% API
@@ -22,7 +30,8 @@
 -spec init() -> ok.
 init() ->
   ets:new(?FUNCTIONS_STORE_NAME, [set, named_table]),
-  ets:new(?VALUES_STORE_NAME, [set, named_table]).
+  ets:new(?VALUES_STORE_NAME, [set, named_table]),
+  ets:new(?VECTORS_STORE_NAME, [set, named_table]).
 
 -spec get_mfa(atom()) -> {ok, mfa()} | not_found.
 get_mfa(FunName) ->
@@ -39,12 +48,52 @@ set_mfa(FunName, MFA) ->
 
 -spec get_val(atom()) -> {ok, term()} | not_found.
 get_val(Key) ->
-  ok.
+  try ets:lookup(?VALUES_STORE_NAME, Key) of
+    [{Key, Val}] -> {ok, Val};
+    [] -> not_found
+  catch
+    error:badarg -> not_found
+  end.
 
 -spec set_val(atom(), term()) -> ok.
 set_val(Key, Val) ->
-  ok.
+  ets:insert(?VALUES_STORE_NAME, {Key, Val}).
+
+-spec init_vector(non_neg_integer()) -> vector().
+init_vector(Length) ->
+  Id = rand_id(),
+  ets:insert(?VECTORS_STORE_NAME, {Id, {Length, #{}}}),
+  Id.
+
+-spec set_vector_val(vector(), non_neg_integer(), term()) -> ok | invalid_index | not_found.
+set_vector_val(Id, Index, Val) ->
+  try ets:lookup(?VECTORS_STORE_NAME, Id) of
+    [{Id, {Length, Vec}}] when Index < Length ->
+      ets:insert(?VECTORS_STORE_NAME, {Id, {Length, Vec#{Index => Val}}}),
+      ok;
+    [{Id, {_Size, _Vec}}] -> invalid_index;
+    [] -> not_found
+  catch
+    error:badarg -> not_found
+  end.
+
+-spec get_vector_val(vector(), non_neg_integer()) -> {ok, term()} | invalid_index | not_found.
+get_vector_val(Id, Index) ->
+  try ets:lookup(?VECTORS_STORE_NAME, Id) of
+    [{Id, {Length, Vec}}] when Index < Length ->
+      case maps:get(Index, Vec, undefined) of
+        undefined -> invalid_index;
+        Val -> {ok, Val}
+      end;
+    [{Id, {_Length, _Vec}}] -> invalid_index;
+    [] -> not_found
+  catch
+    error:badarg -> not_found
+  end.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+rand_id() ->
+  rand:uniform(?VECTOR_MAX_ID).
